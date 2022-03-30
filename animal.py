e0e69f44ba0dc363b29c7424dd5971efc937abdc
GarrettBreard <100252661+GarrettBreard@users.noreply.github.com>
2022-03-29 22:21:04 -0500

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Garrett Breard
+# GitHub username: garrettbreard
+# Date: 3/29/2022
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
